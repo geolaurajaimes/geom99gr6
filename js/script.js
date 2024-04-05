6e8ea0705ea274c671a3283bdaab42c94030b63b
Laura Jaimes <146447166+geolaurajaimes@users.noreply.github.com>
2024-04-05 17:34:34 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 /* https://www.w3schools.com/bootstrap/bootstrap_ref_js_dropdown.asp */
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("dropdownBtn").addEventListener("click", toggleDropdown);
-});
-
-function toggleDropdown() {
-    var dropdown = document.getElementById("myDropdown");
-    if (dropdown.style.display === "none" || dropdown.style.display === "") {
-        dropdown.style.display = "block";
-    } else {
-        dropdown.style.display = "none";
+function toggleDropdown(className) {
+    var dropdowns = document.getElementsByClassName(className);
+    for (var i = 0; i < dropdowns.length; i++) {
+        var dropdown = dropdowns[i];
+        if (dropdown.style.display === "none" || dropdown.style.display === "") {
+            dropdown.style.display = "block";
+        } else {
+            dropdown.style.display = "none";
+        }
     }
 }
